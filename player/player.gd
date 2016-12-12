be7af435096ea7613c0458ee8f0d26028de362f7
Eshan Das <eshandasnit@gmail.com>
2016-12-13 00:01:54 +0530

    Fiduring out movement

--- player/player.gd
+++ player/player.gd
@@ -13,7 +13,7 @@ class BackEngine:
 
 class FrontBooster:
 	var pos = null
-	var thrust = Vector2(0, 2)
+	var thrust = Vector2(0, 10)
 	var emitter = null
 
 
@@ -36,6 +36,8 @@ var right_booster = RightBooster.new()
 var left_booster = LeftBooster.new()
 
 func _ready():
+	set_rotd(90)
+	var alpha = get_rot()
 	# Get all the emitters and assign to engines
 	back_engine.emitter = get_node("back_engine")
 	back_engine.pos = back_engine.emitter.get_pos()
@@ -45,25 +47,29 @@ func _ready():
 	right_booster.pos = right_booster.emitter.get_pos()
 	left_booster.emitter = get_node("left_booster")
 	left_booster.pos = left_booster.emitter.get_pos()
+	
+	var new_x = (back_engine.thrust.x * cos(alpha)) + (back_engine.thrust.y * sin(alpha))
+	var new_y = (-back_engine.thrust.x * sin(alpha)) + (back_engine.thrust.y * cos(alpha))
+	back_engine.thrust = Vector2(new_x, new_y)
 	set_fixed_process(true)
 
 
 func _fixed_process(delta):
-	var cur_x
-	var cur_y
-	var temp_x
-	var temp_y
-	get_node("label").set_text(str(get_rotd()))
+	var alpha = get_rotd()
+#	var new_x = (back_engine.thrust.x * cos(alpha)) + (back_engine.thrust.y * sin(alpha))
+#	var new_y = (-back_engine.thrust.x * sin(alpha)) + (back_engine.thrust.y * cos(alpha))
+	get_node("label").set_text("%s, %s, %s" % [alpha, sin(get_rot()), back_engine.thrust])
 	if(Input.is_action_pressed("ui_up")):
 		back_engine.emitter.set_emitting(true)
-		apply_impulse(back_engine.pos, back_engine.thrust)
+		
+		apply_impulse(Vector2(0, 0), back_engine.thrust)
 	elif(Input.is_action_pressed("ui_down")):
 		front_booster.emitter.set_emitting(true)
 		apply_impulse(front_booster.pos, front_booster.thrust)
-	elif(Input.is_action_pressed("ui_right")):
+	elif(Input.is_action_pressed("ui_left")):
 		right_booster.emitter.set_emitting(true)
 		apply_impulse(right_booster.pos, right_booster.thrust)
-	elif(Input.is_action_pressed("ui_left")):
+	elif(Input.is_action_pressed("ui_right")):
 		left_booster.emitter.set_emitting(true)
 		apply_impulse(left_booster.pos, left_booster.thrust)
 	else:
@@ -71,4 +77,3 @@ func _fixed_process(delta):
 		front_booster.emitter.set_emitting(false)
 		right_booster.emitter.set_emitting(false)
 		left_booster.emitter.set_emitting(false)
-
